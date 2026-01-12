eb4ea557132dce6989b6c7768560dab83386f3a0
Mandeep Nehra <207450546+Mandeep-codes@users.noreply.github.com>
2026-01-12 16:36:21 +0530

    client adding

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import {Route, Routes} from "react-router-dom";
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='relative sm: -8 p-4 bg-[#13131a] min-h-screen flex flex-row'>
+      <div className='sm:flex hidden mr-10 relative'>
+        Sidebar
+      </div>
+    </div>
   )
 }
 
