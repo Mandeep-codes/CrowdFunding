e690a6ffa3d8ba8c31c4d298b2750106426335f2
Mandeep Nehra <207450546+Mandeep-codes@users.noreply.github.com>
2026-01-13 13:10:44 +0530

    day-2

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,13 +1,40 @@
-import React,{useState} from 'react'
-import {Link, NavLink} from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-import {logo,sun} from "../assets";
-import {navlinks} from "../constants";
+import { logo, sun } from "../assets";
+import { navlinks } from "../constants";
 
-function Sidebar() {
+const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
+  <div
+    className={`w-[48px] h-[48px] rounded-[10px] 
+    ${isActive === name && "bg-[#2c2f32]"} 
+    flex justify-center items-center 
+    ${!disabled && "cursor-pointer"} 
+    ${styles}`}
+    onClick={handleClick}
+  >
+    <img
+      src={imgUrl}
+      alt="icon"
+      className={`w-1/2 h-1/2 ${isActive !== name && "grayscale"}`}
+    />
+  </div>
+);
+
+const Sidebar = () => {
+  const navigate = useNavigate();
+  const [isActive, setIsActive] = useState("dashboard");
+
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
+      <Link to="/">
+        <Icon
+          styles="w-[52px] h-[52px] bg-[#2c2f32]"
+          imgUrl={logo}   
+        />
+      </Link>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
