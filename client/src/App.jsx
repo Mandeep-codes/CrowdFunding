e6efe5b0fa8b5773b76f8503b52332a6b958a338
Mandeep Nehra <207450546+Mandeep-codes@users.noreply.github.com>
2026-01-15 19:07:23 +0530

    day-4

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,27 @@
-import React from 'react'
-import {Route, Routes} from "react-router-dom";
-
-import {Navbar, Sidebar} from "./components";
-import {Home, Profile, CreateCompaign, CompaignDetails} from "./pages";
+import React from 'react';
+import { Route, Routes } from "react-router-dom";
+import { Navbar, Sidebar } from "./components";
+import { Home } from "./pages";
 
 const App = () => {
   return (
-    <div className='relative sm: -8 p-4 bg-[#13131a] min-h-screen flex flex-row'>
-      <div className='sm:flex hidden mr-10 relative'>
-        < Sidebar />
+    <div className="relative p-4 bg-[#13131a] min-h-screen flex flex-row">
+      <div className="sm:flex mr-10 relative">
+        <Sidebar />
       </div>
 
-      <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
-        < Navbar />
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        <Navbar />
 
         <Routes>
-          <Route path='/' element={<div>Home</div>}/>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/profile" element={<Profile />} />
+          <Route path="/create-campaign" element={<CreateCampaign />} />
+          <Route path="/campaign-details/:id" element={<CampaignDetails />} /> */}
         </Routes>
-          
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,2 +1,3 @@
 export {default as Navbar} from "./Navbar.jsx";
-export {default as Sidebar} from "./Sidebar.jsx"
+export {default as Sidebar} from "./Sidebar.jsx";
+export {default as CustomButton} from "./CustomButton.jsx";
